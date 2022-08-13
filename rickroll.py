6919cc365d0b9a7fa6a5f70a4fc21069bdfed208
MightySpaceman <83145315+MightySpaceman@users.noreply.github.com>
2022-08-13 21:30:37 +1000

    Add files via upload

---
+++ rickroll.py
@@ -0,0 +1,8 @@
+import ctypes
+import time
+import subprocess
+import os
+os.system('curl -o C:\\tmp\\rick.jpeg https://i.imgur.com/VmlIAvj.jpeg/echo')
+os.system('start https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+ctypes.windll.user32.SystemParametersInfoW(20, 0, "C:\\tmp\\rick.jpeg" , 0)
+
